22d3852749324e002628d27f93c1744a8937bc4b
SB <smpalasis@gmail.com>
2024-10-01 22:13:33 +0300

    Time: 2 ms (35.57%), Space: 7.5 MB (26%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n < 0)
+        {
+            return false;
+        }
+        return (( (n & (n-1)) == 0 ) && ((n & 0x55555555) != 0));
+    }
+};
